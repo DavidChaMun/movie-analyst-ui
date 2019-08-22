b27f906f7dfed48c328a59f1b02afae291f95b22
David <david.loc.ch@gmail.com>
2019-08-22 15:39:58 -0500

    Fixed deploy script error

--- deploy_ui.sh
+++ deploy_ui.sh
@@ -2,7 +2,7 @@
 cd $(dirname $0)
 docker build -t movie_analyst_ui .
 host=$(docker exec -it malyst_apic01 bash -c 'IP=$(hostname -i); echo $IP')
-docker run --name malyst_uic01 -e BACK_HOST=host -d movie_analyst_ui
+docker run --name malyst_uic01 -e BACK_HOST=$host -d movie_analyst_ui
 echo ""
 echo -n "Movie Anlayst UI running on: "
 echo -n $(docker exec -it malyst_uic01 bash -c 'IP=$(hostname -i); echo $IP')
