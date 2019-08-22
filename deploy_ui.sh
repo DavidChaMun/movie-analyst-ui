baad46e088abb1434f564c66231537572a5a82ba
David <david.loc.ch@gmail.com>
2019-08-22 17:01:48 -0500

    Fixed invisible error

--- deploy_ui.sh
+++ deploy_ui.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd $(dirname $0)
 docker build -t movie_analyst_ui .
-host=$(docker exec -it malyst_apic01 bash -c 'IP=$(hostname -i); echo $IP')
-docker run --name malyst_uic01 -e BACK_HOST=$host -d movie_analyst_ui
+host_dir=$(docker exec -it malyst_apic01 bash -c 'IP=$(hostname -i); echo $IP')
+host_dir=$(echo $host_dir | sed -e 's/\n//g')
+host_dir=$(echo $host_dir | sed -e 's/\r//g')
+docker run --name malyst_uic01 -e BACK_HOST=$host_dir -d movie_analyst_ui
 echo ""
 echo -n "Movie Anlayst UI running on: "
 echo -n $(docker exec -it malyst_uic01 bash -c 'IP=$(hostname -i); echo $IP')
